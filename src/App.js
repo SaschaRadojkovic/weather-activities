9c0c48767a374ffb216c8956f1ce68331da967a4
sascha radojkovic <scha13@gmx.de>
2022-12-20 14:08:49 +0100

    add form.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form/Form";
+import { uid } from "uid";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivitiy({ activity, isForGoodWeather = false }) {
+    console.log("act", activities);
+    setActivities([...activities, { id: uid(), activity, isForGoodWeather }]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form onAddActivity={handleAddActivitiy} />
+    </>
   );
 }
 
